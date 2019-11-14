bf12bce6dd54cf7a2cd05065ceb51dc87b4ca48e
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-11-14 19:01:57 +0300

    8. Правильный вариант

--- testPow.js
+++ testPow.js
@@ -1,6 +1,8 @@
 describe("pow", function() {
-    it("возводит в степень n", function() {
+    it("2 в степени 3 будет 8", function() {
         assert.equal(pow(2, 3), 8);
-        assert.equal(pow(3, 4), 81);
-    })
+    });
+    it("3 в степени 3 будет 27", function() {
+        assert.equal(pow(3, 3), 27);
+    });
 });
