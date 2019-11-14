579e7e9a7ae868ba9597f832a649bead18ea96f2
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-11-14 19:06:42 +0300

    9. Улучшаем реализацию

--- testPow.js
+++ testPow.js
@@ -1,8 +1,15 @@
 describe("pow", function() {
-    it("2 в степени 3 будет 8", function() {
-        assert.equal(pow(2, 3), 8);
-    });
-    it("3 в степени 3 будет 27", function() {
-        assert.equal(pow(3, 3), 27);
-    });
+
+    function makeTest(x) {
+        let expected = x * x * x;
+
+        it(`${x} в степени 3 будет ${expected}`, function() {
+            assert.equal(pow(x, 3), expected);
+        })
+    }
+
+    for (let x = 1; x <= 5; x++) {
+        makeTest(x);
+    }
+
 });
